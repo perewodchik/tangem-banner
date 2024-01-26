211821232f066474f982cd3de809b70478bc251d
Vladislav <it.smirnov.vladislav@yandex.ru>
2024-01-26 23:20:29 +0700

    fix: убран импорт

--- src/entities/banner/top-banner/ui/organisms/top-banner/top-banner.tsx
+++ src/entities/banner/top-banner/ui/organisms/top-banner/top-banner.tsx
@@ -1,4 +1,4 @@
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
 
 import { useDeviceSize } from "@/shared/theme";
 import { CloseButton } from "@/shared/ui/atoms";
